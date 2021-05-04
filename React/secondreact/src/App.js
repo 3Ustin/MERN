5502cd025ab41a0cf0164a2457d8b5bcb83ec05a
Austin D Dupras <79591045+3Ustin@users.noreply.github.com>
2021-05-04 19:59:59 -0400

    react projects

---
+++ React/secondreact/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Hello PEEPS I GOT IT WORKING YO</h1>
+    <h3>I'm feeling pretty proud here.</h3>
+    <p>I mean I never thought this it'd be easy</p>
+    <p>now I've made me third first react project</p>
+    <p>so yeah</p>
+    <p>I got me some great teachers yo</p>\
+    <h1>YO!</h1>
+    </div>
+  );
+}
+
+export default App;
