06b967945afa44921dcdc5cb460af0ec9b0f1510
Austin D Dupras <79591045+3Ustin@users.noreply.github.com>
2021-05-06 00:37:37 -0400

    moreForms

---
+++ React/moreforms/src/components/PersonCard.js
@@ -0,0 +1,51 @@
+import React, { Component, useState } from 'react';
+
+const PersonCard  = props => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [hasBeenSubmitted, setHasBeenSubmitted] = useState(false);
+
+    const createUser = (e) => {
+        e.preventDefault();
+        const newUser = { username, password, confirmPassword };
+        console.log("Welcome", newUser);
+        setHasBeenSubmitted( true );
+    };
+
+    const formMessage = () => {
+        if( hasBeenSubmitted ){
+            return "Thank you for submitting the form!";
+        } else {
+            return "Welcome, please submit the form";
+        }
+    };
+
+    return(
+        <div>
+            <h3>{formMessage}</h3>
+            <form onSubmit={ createUser }>
+                <div>
+                    <label>Username: </label> 
+                    <input type="text" onChange={ (e) => setUsername(e.target.value) } />
+                </div>
+                <div>
+                    <label>Password: </label> 
+                    <input type="text" onChange={ (e) => setPassword(e.target.value) } />
+                </div>
+                <div>
+                    <label>Confirm Password: </label> 
+                    <input type="text" onChange={ (e) => setConfirmPassword(e.target.value) } />
+                </div>
+                <input type="submit" value="Create User" />
+            </form>
+            <div>
+                <h3> {username} </h3>
+                <h3> {password} </h3>
+                <h3> {confirmPassword} </h3>
+            </div>
+        </div>
+    );
+};
+
+export default PersonCard;
